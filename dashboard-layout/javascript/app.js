1fbbeb9130719bf98fa76aa8d4ab84a3d9a02740
ElliotFer2000 <52060565+ElliotFer2000@users.noreply.github.com>
2023-06-05 11:48:41 -0600

    add dist files

--- dashboard-layout/javascript/app.js
+++
@@ -1,7 +0,0 @@
-import "../css/materialize.css";
-import "../css/backoffice.css";
-import "./materialize.js";
-import "./MenuProfile.js";
-import "./SideNav.js";
-
-const instaceAccordion = new M.Collapsible(document.querySelector("#collapsible"))
--- spa-template/javascript/Sidenav.js
+++
@@ -1,8 +0,0 @@
-
-function MainSidenav() {
-    
-    const instance = M.Sidenav.init(document.querySelector(".header__sidenav"),{edge: 'right'});
-    return instance
-}
-
-export {MainSidenav}
