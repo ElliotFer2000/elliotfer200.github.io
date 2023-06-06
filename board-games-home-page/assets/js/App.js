005f8967ad4fc0b02d9ec0fce7c94a45d2b92fc0
ElliotFer2000 <52060565+ElliotFer2000@users.noreply.github.com>
2023-06-06 16:32:16 -0600

    add dist files

--- board-games-home-page/assets/js/App.js
+++ board-games-home-page/assets/js/App.js
@@ -10092,7 +10092,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.footerObserver = void 0;
 function testViewportForFooter(entries, observer) {
   if (bottomNav.offsetWidth !== 0) {
-    entries[0].isIntersecting ? bottomNav.classList.add("nav__invisible") : bottomNav.classList.remove("nav__invisible");
+    entries[0].isIntersecting ? bottomNav.classList.toggle("nav__invisible") : bottomNav.classList.toggle("nav__invisible");
   }
 }
 var footerObserver = new IntersectionObserver(testViewportForFooter);
